fe4e47e79771efe66bea9c92ab1466b39e662c00
Arul Suju <arulsuju@yahoo.co.in>
2018-05-30 22:35:47 +0530

    Initial commit

---
+++ src/main/java/come/suju/web1/web/WebApplication.java
@@ -0,0 +1,12 @@
+package come.suju.web1.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
